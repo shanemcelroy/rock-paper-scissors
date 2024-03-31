9ce1351f8a2c88b92b975a6e14022c182e425dab
Shane McElroy <mcelroysh@gmail.com>
2024-03-31 15:49:40 -0700

    Make the game replayable up to 5 rounds
    To make the game replayable, the user and computer choices were
    moved inside the playRound function, the parameters were deleted,
    and a for loop was added in the playGame function to play 5 rounds.

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,15 @@
-//Create an array of choices for the computer 
-const choices = ["rock", "paper", "scissors"];
-//Store a random number between 0 and the length of the array to use as an index for the computers choice
-let randInt = Math.floor(Math.random() * 3)
-//Create a getComputerChoice variable that randomly selects an item in the array
-let getComputerChoice = choices[randInt]
-//Create a getUserChoice variable that stores the user input of Rock, Paper, or Scissors 
-let getUserChoice = prompt("Choose 'Rock', 'Paper', or 'Scissors': ").toLowerCase()
 //Create a playRound function that takes the user and computer choice as parameters and returns a string if you win or lose
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock') {
+function playRound() {
+    //Create an array of choices for the computer 
+    const choices = ["rock", "paper", "scissors"];
+    //Store a random number between 0 and the length of the array to use as an index for the computers choice
+    let randInt = Math.floor(Math.random() * 3)
+    //Create a getComputerChoice variable that randomly selects an item in the array
+    let getComputerChoice = choices[randInt]
+    //Create a getUserChoice variable that stores the user input of Rock, Paper, or Scissors 
+    let getUserChoice = prompt("Choose 'Rock', 'Paper', or 'Scissors': ").toLowerCase()
+
+    if (getUserChoice === 'rock') {
         switch (getComputerChoice) {
             case 'paper':
                 return "You lose! Paper beats rock!";
@@ -17,7 +18,7 @@ function playRound(playerSelection, computerSelection) {
             default:
                 return "You tied! You both chose rock!";
         }
-    } else if (playerSelection === 'paper') {
+    } else if (getUserChoice === 'paper') {
         switch (getComputerChoice) {
             case 'rock':
                 return "You win! Paper beats rock!";
@@ -26,7 +27,7 @@ function playRound(playerSelection, computerSelection) {
             default:
                 return "You tied! You both chose paper!";
         }
-    } else if (playerSelection === 'scissors') {
+    } else if (getUserChoice === 'scissors') {
         switch (getComputerChoice) {
             case 'rock':
                 return "You lose! Rock beats scissors!";
@@ -37,8 +38,22 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 }
-playRound(getUserChoice, getComputerChoice)
-//Wrap the playRound function into a playGame function to play a 5-round game
+
+//Create a playGame function to play a 5-round game
     //Create variables playerScore, computerScore, and roundNumber 
     //After each round is played, add a point to either the player or computer, and increment round by 1
+    //Display the score after each round
     //After the fifth round, return a string of the player with the highest score
+
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let round = 1;
+
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound())
+        round++;
+        console.log(i)
+    }
+}
+playGame()
